d26b882ea79636f92068eef5f049dc1d755ca451
Thomas Patrick Levy <thomasplevy@gmail.com>
2017-08-16 17:31:30 -0700

    1.3.1

--- lifterlms-labs.php
+++ lifterlms-labs.php
@@ -3,7 +3,7 @@
  * Plugin Name: LifterLMS Labs
  * Plugin URI: https://lifterlms.com/
  * Description: Experimental, conceptual, and possibly silly new features to improve and enhance the functionality of the LifterLMS core
- * Version: 1.3.0
+ * Version: 1.3.1
  * Author: Thomas Patrick Levy, codeBOX LLC
  * Author URI: http://gocodebox.com
  * Text Domain: lifterlms-labs
@@ -11,7 +11,7 @@
  * License:     GPLv2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Requires at least: 4.0
- * Tested up to: 4.7.1
+ * Tested up to: 4.8.1
  */
 
 /**
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 final class LifterLMS_Labs {
 
-	public $version = '1.3.0';
+	public $version = '1.3.1';
 
 	protected static $_instance = null;
 
